dcf2c02624ba33357673e8b2863e440c794ad91f
Jessie Tivey <jesstivey@gmail.com>
2020-04-22 09:09:52 -0400

    Completed exercise 15

---
+++ 09-exercises/09-15.rb
@@ -0,0 +1,14 @@
+# What will the following program output?
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+
+# These hashes are the same! 
+
+# Got this one wrong. :( I had a 50/50 shot!
