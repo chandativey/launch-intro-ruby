854cb6483fa9a5753c5dba3c2c086be3d0cce9de
Jessie Tivey <jesstivey@gmail.com>
2020-06-15 08:32:43 -0400

    Going through Intro to Ruby book again; moved first go thru into its own folder and created a new one for 2nd time thru

---
+++ 2nd-time-thru/01-the-basics/03-movies-hash.rb
@@ -0,0 +1,13 @@
+# Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen.
+
+movies = {  one: 1975,
+            two: 2004,
+            three: 2013,
+            four: 2001,
+            five: 1981 }
+
+p movies[:one]
+p movies[:two]
+p movies[:three]
+p movies[:four]
+p movies[:five]
---
+++ 2nd-time-thru/01-the-basics/04-movies-array.rb
@@ -0,0 +1,9 @@
+# Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+
+movies = [["one", 1975], ["two", 2004], ["three", 2013], ["four", 2001], ["five", 1981]]
+
+p movies[0][1]
+p movies[1][1]
+p movies[2][1]
+p movies[3][1]
+p movies[4][1]
